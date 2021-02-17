9785f5516264b0457750d85a41a125d0c7a76e13
Francesco Turci <turci@me.com>
2021-02-17 18:18:14 +0000

    notebooks

--- src/image_to_numpy.py
+++ src/image_to_numpy.py
@@ -4,20 +4,30 @@ import skimage
 import scipy
 import copy
 from scipy import ndimage
-data = skimage.io.imread("../exp-data/0.524squ.png").astype(float)
 
-plt.imshow(data)
+class Reader:
 
-sx, sy = 30,70
-kernel = copy.deepcopy(data[sx:sx+128,sy:sy+128])
+    def __init__(self,path = "../exp-data/0.524squ.png"):
+        self.image = skimage.io.imread(path).astype(float)
+        fig, ax = plt.subplots(1,2, figsize=(12,5))
+        ax[0].matshow(self.image, cmap = plt.cm.binary)
+        ax[1].hist(self.image.ravel(), bins=16)
+        ax[1].set_xlabel('intensity')
+      
+        
+    def get_kernel(self,si=30,sj=70, width=128, smoothing=1.0):
+        """Extract a kernel, remove the average, compute the fourier transform and zero regions where the power spectrum is below some standard deviations from the average"""
+        kernel = copy.deepcopy(self.image[si:si+width,sj:sj+width])
+        self.width = width
+        kernel -= kernel.mean()
 
-kernel -= kernel.mean()
-kernel = ndimage.gaussian_filter(kernel, 1.0)
-kernel [kernel<kernel.mean()]=0
-plt.figure()
-plt.pcolormesh(scipy.fft.fftshift(np.absolute(scipy.fft.fft2(kernel))))
+        smooth_kernel = ndimage.gaussian_filter(kernel, smoothing)
+        # smooth_kernel [smooth_kernel<(smooth_kernel.mean()-smooth_kernel.std()*zero_below)]=smooth_kernel.min()
+        ftkernel = scipy.fft.fft2(smooth_kernel)
 
-np.save("../exp-data/0.524squ.npy", data)
-np.save("../exp-data/kernel.npy", kernel)
-
-plt.show()
+        
+        fig, ax = plt.subplots(1,3,figsize=(12,5))
+        ax[0].matshow(kernel, cmap = plt.cm.binary)
+        ax[1].matshow(smooth_kernel, cmap = plt.cm.binary)
+        ax[2].matshow(scipy.fft.fftshift(np.absolute(ftkernel)),cmap = plt.cm.binary)
+        self.kernel = smooth_kernel
